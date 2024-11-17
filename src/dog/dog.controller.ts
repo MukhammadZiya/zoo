5547d52741f8099a994e0712984839c97501e02c
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-11-18 01:35:40 +0900

    feat: initial setup

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { DogService } from './dog.service';
+import { query, Request } from 'express';
+
+@Controller('dog')
+export class DogController {
+  constructor(private readonly dogService: DogService) {}
+
+  @Get()
+  public getHello(): string {
+    return this.dogService.getHello();
+  }
+
+  @Get('greet/:id')
+  public introduce(@Param() params: any, @Query() query: any): string {
+    // console.log("params", request.params)
+    console.log('quesry', query)
+    console.log('id', params);
+    return this.dogService.introduce();
+  }
+}
---
+++ src/dog/dog.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DogService {
+  public getHello(): string {
+    return 'WOWWOW';
+  }
+
+  public introduce(): string {
+    return "My name is Jerry"
+  }
+}
